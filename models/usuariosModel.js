5fa8cc2b2b96357ce42262532dd6d44bebb982d9
nicholasmillani <nicholas.millani@sou.inteli.edu.br>
2025-06-03 09:39:24 -0300

    arrumando comentarios

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -20,7 +20,7 @@ class Usuario{
 
     static async criarUsuario(nome, email, senha) {
         try {
-            const saltRounds = 10; // padrão recomendado
+            const saltRounds = 10;
             const senhaHash = await bcrypt.hash(senha, saltRounds);
 
             const result = await db.query(
@@ -52,7 +52,7 @@ class Usuario{
     }
     static async autenticarUsuario(email, senha){
          try {
-        // 1. Buscar usuário pelo email
+            //Buscar usuário pelo email
             const result = await db.query('SELECT * FROM usuarios WHERE email = $1', [email]);
             
             if (result.rows.length === 0) {
@@ -60,10 +60,10 @@ class Usuario{
             }
             const usuario = result.rows[0];
 
-            // 2. Comparar senha digitada com a senha criptografada do banco
+            //Comparar senha digitada com a senha criptografada do banco
             const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
 
-            // 3. Retornar usuário se a senha estiver correta
+            //Retornar usuário se a senha estiver correta
             return senhaCorreta ? usuario : null;
 
         } 
